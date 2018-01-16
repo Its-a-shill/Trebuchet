b6fc51c752e8e57e0415cc6a20cc455ee9268fb2
Stuart <stuartnevlocke@gmail.com>
2018-01-16 12:05:59 -0500

    Update

---
+++ Trebuchet.java
@@ -0,0 +1,17 @@
+
+public class Trebuchet{
+	
+	public static void main(String[] args){
+		
+	}
+	
+	static class TrebuchetProperties{
+		double distanceToTarget;
+		final double projectileMass = 1;
+		final double armDensity = 1;
+		double startingAngle;
+		double releaseAngle;
+		
+		
+	}
+}
